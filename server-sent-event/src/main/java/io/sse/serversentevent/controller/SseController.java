d1fd9125ed67c43ecedfb9bfb534cbfaf9d0b3e8
gimeast <dudu.gimeast@gmail.com>
2024-06-29 22:29:51 +0900

    SSE를 이용한 메세지 전송

--- server-sent-event/src/main/java/io/sse/serversentevent/controller/SseController.java
+++ server-sent-event/src/main/java/io/sse/serversentevent/controller/SseController.java
@@ -1,17 +1,24 @@
 package io.sse.serversentevent.controller;
 
+import io.sse.serversentevent.SseEmitters;
 import io.sse.serversentevent.service.SseService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/sse")
 public class SseController {
 
-    @Autowired
-    private SseService sseService;
+    private final SseEmitters sseEmitters;
+    private final SseService sseService;
 
     @GetMapping(path = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(){
@@ -20,6 +27,20 @@ public class SseController {
         sseService.addEmitter("tester", emitter);
         sseService.sendEvents();
         return emitter;
+    }
+
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect() {
+        SseEmitter emitter = new SseEmitter();
+        sseEmitters.add(emitter);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("connect")
+                    .data("connected!"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
     }
 
 }
